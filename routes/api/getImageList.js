2179be83ba04565061f4c5929b431cffaeb2d08c
fanzehua <316679581@qq.com>
2019-06-27 11:38:55 +0800

    添加获取图片列表接口

---
+++ routes/api/getImageList.js
@@ -0,0 +1,51 @@
+let express = require('express');
+let router = express.Router();
+let User = require('../../models/user.js');
+let Image = require('../../models/image.js');
+let _ = require('lodash');
+
+router.post('/', async (req, res, next) => {
+    let { userId, pageNo, pageSize } = req.body;
+    User.findOne({ '_id': userId }, (err, doc) => {
+        if (err) {
+            res.json({
+                success: false,
+                msg: '账号有误，请重新登录'
+            })
+        } else {
+            let user = doc.toObject();
+            if (_.findIndex(user.permissions, o => {
+                return o === 'watchImage';
+            }) !== -1) {
+                Image.countDocuments({}, (err1, num) => {
+                    if (err1) {
+                        res.json({
+                            success: false,
+                            msg: err1.message
+                        })
+                    } else {
+                        Image.find().skip(pageNo * pageSize)
+                            .limit(pageSize).exec((err2, docs) => {
+                                let data = [];
+                                docs.forEach(doc => {
+                                    data.push(doc);
+                                })
+                                res.json({
+                                    success: true,
+                                    count: num,
+                                    data
+                                })
+                            })
+                    }
+                });
+            } else {
+                res.json({
+                    success: false,
+                    msg: '账号权限不足，请联系管理员'
+                })
+            }
+        }
+    });
+});
+
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const userUpdate = require("./api/userUpdate.js");
 const modifyPassword = require("./api/modifyPassword.js");
 const getWeaths = require("./api/getWeaths.js");
 const getknowledge = require("./api/getknowledge.js");
+const getImageList = require("./api/getImageList.js");
 
 router.use("/register", register);
 router.use("/login", login);
@@ -14,6 +15,7 @@ router.use("/userUpdate", userUpdate);
 router.use("/getWeaths", getWeaths);
 router.use("/modifyPassword", modifyPassword);
 router.use("/getknowledge", getknowledge);
+router.use("/getImageList", getImageList);
 
 
 module.exports = router;
